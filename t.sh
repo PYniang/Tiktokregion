d2ad69a8e03772d8f81a3a1af8b7a911fcbe7855
PYniang <45135188+PYniang@users.noreply.github.com>
2022-11-12 21:08:06 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,10 +1,22 @@
+checkOS() {
+    if [ "$(which apt 2>/dev/null)" ]; then
+        InstallMethod="apt"
+        is_debian=1
+    elif [ "$(which dnf 2>/dev/null)" ] || [ "$(which yum 2>/dev/null)" ]; then
+        InstallMethod="yum"
+        is_redhat=1
+    fi
+}
+checkOS
+
 if [ ! "$(command -v jq)" ]; then
-  apt install jq -y
-  yum install jq -y
+  if [ "$InstallMethod"=="yum" ]; then
+    $InstallMethod install epel-release -y
+  fi
+   $InstallMethod install jq -y
 fi
 if [ ! "$(command -v curl)" ]; then
-  apt install curl -y
-  yum install curl -y
+  $InstallMethod install curl -y
 fi
 clear
 region=$(curl -s https://www.tiktok.com/node/common/web-privacy-config?locale=zh-Hant-TW | jq '.body.appProps.region')
